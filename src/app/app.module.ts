644ce7c984d684c37c0e12df5df5676b08eba5d4
Alberto Real Estepa <Alberto.Real@dxcfds.com>
2019-10-12 14:04:25 +0200

    login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ArchitectureComponent } from './architecture/architecture.component';
 import { SummaryComponent } from './summary/summary.component';
 import { LoginComponent } from './login/login.component';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
