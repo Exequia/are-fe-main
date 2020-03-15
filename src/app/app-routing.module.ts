bde1d2ede77b787e3751052f07e82eb0cb942604
Alberto Real <Alberto.Real@dxcfds.com>
2020-03-15 18:35:57 +0100

    fix: project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { HomeComponent } from "./template/home/home.component";
+import { PageNotFoundComponent } from "./template/page-not-found/page-not-found.component";
 import { AuthGuardService } from "./services/auth-guard.service";
 
 const routes: Routes = [
---
+++ src/app/template/summary/summary.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { environment } from "../../../environments/environment";
+
+@Component({
+  selector: "app-summary",
+  templateUrl: "./summary.component.html",
+  styleUrls: ["./summary.component.scss"]
+})
+export class SummaryComponent implements OnInit {
+  constructor(private translate: TranslateService) {}
+
+  private subject = "";
+
+  ngOnInit() {
+    this.getSubject();
+  }
+
+  getAuthor(): string {
+    return environment.author;
+  }
+
+  getSubject() {
+    this.translate.get("contact.subject").subscribe((res: string) => {
+      this.subject = res;
+    });
+  }
+
+  getEmail(): string {
+    return environment.email;
+  }
+
+  getMailTo(): string {
+    return `mailto:${environment.email}?Subject=${this.subject}`;
+  }
+}
