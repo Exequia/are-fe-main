fabb3684d57daf591319966864edbff7240ebf3d
Alberto Real <Alberto.Real@dxcfds.com>
2020-04-30 18:58:26 +0200

    fix: game board

--- src/app/games/views/three-in-line/three-in-line.component.spec.ts
+++ src/app/games/views/three-in-line/three-in-line.component.spec.ts
@@ -1,41 +1,36 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ThreeInLineComponent } from "./three-in-line.component";
-import { ThreeInLineGameComponent } from "../../component/three-in-line-game/three-in-line-game.component";
-import { ThreePlayerComponent } from "../../component/three-player/three-player.component";
-import { ThreeDashboardComponent } from "../../component/three-dashboard/three-dashboard.component";
+import { ThreeInLineComponent } from './three-in-line.component';
+import { ThreeInLineGameComponent } from '../../component/three-in-line-game/three-in-line-game.component';
+import { ThreePlayerComponent } from '../../component/three-player/three-player.component';
+import { ThreeDashboardComponent } from '../../component/three-dashboard/three-dashboard.component';
 
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
-import { TranslateHttpLoader } from "@ngx-translate/http-loader";
-import { HttpClient } from "@angular/common/http";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
 }
 
-describe("ThreeInLineComponent", () => {
+describe('ThreeInLineComponent', () => {
   let component: ThreeInLineComponent;
   let fixture: ComponentFixture<ThreeInLineComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        ThreeInLineComponent,
-        ThreeInLineGameComponent,
-        ThreePlayerComponent,
-        ThreeDashboardComponent,
-      ],
+      declarations: [ThreeInLineComponent, ThreeInLineGameComponent, ThreePlayerComponent, ThreeDashboardComponent],
       imports: [
         HttpClientTestingModule,
         TranslateModule.forRoot({
           loader: {
             provide: TranslateLoader,
             useFactory: HttpLoaderFactory,
-            deps: [HttpClient],
-          },
-        }),
-      ],
+            deps: [HttpClient]
+          }
+        })
+      ]
     }).compileComponents();
   }));
 
@@ -45,7 +40,7 @@ describe("ThreeInLineComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/games/views/three-in-line/three-in-line.component.ts
+++ src/app/games/views/three-in-line/three-in-line.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./three-in-line.component.scss']
 })
 export class ThreeInLineComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
